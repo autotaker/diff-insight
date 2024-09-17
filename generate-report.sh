cdff4fc44ec0d7a8b057730c38bf50d13a30536f
autotaker <autotaker@gmail.com>
2024-09-17 09:52:42 +0900

    chore: Update generate-report.sh to use pandoc in Docker container

--- generate-report.sh
+++ generate-report.sh
@@ -19,11 +19,16 @@ pipenv run python src/main.py fetch-commit "${ORG}" "${REPO}" main work/head.jso
 HEAD_SHA=$(jq -r '.sha' work/head.json)
 TODAY=$(date +%Y%m%d)
 
+function run_pandoc_in_docker() {
+  docker run --rm -u "$(id -u):$(id -g)" -v "$(pwd):/data" pandoc/core "$@"
+}
+
 if [ "$LAST_SHA" != "$HEAD_SHA" ]; then
   pipenv run python src/main.py generate-report "${ORG}" "${REPO}" "${LAST_SHA}" "${HEAD_SHA}" work/report-{date}-{category}.md
   for file in work/report-*.md; do
     if [ -f "$file" ]; then
-      pandoc -i "$file" -o "docs/$(basename "$file" .md).html" -f markdown+hard_line_breaks --template template.html
+      # pandoc -i "$file" -o "docs/$(basename "$file" .md).html" -f markdown+hard_line_breaks --template template.html
+      run_pandoc_in_docker -i "$file" -o "docs/$(basename "$file" .md).html" -f markdown+hard_line_breaks --template template.html
     fi
   done
   echo "HEAD_SHA: $HEAD_SHA"
